f4387681efcc594d1a3331fc580be0b2342dd57c
JaceyWebber <jacey.webber@selu.edu>
2022-10-28 16:58:33 +0000

    Initial commit

---
+++ Scripts/Jitterplot.R
@@ -0,0 +1,3 @@
+# Plotting a jitter plot
+
+# Using ggplot2, the geom_jitter function will make a jitter plot of the graph using values in the dataset. 
---
+++ Scripts/Linearmodel.R
@@ -0,0 +1,3 @@
+# Linear Model
+
+# The lm function will create a linear model to explain the relationship between a predictor and response in our dataset. We will be able to see to what degree the predictor determines the response.  
